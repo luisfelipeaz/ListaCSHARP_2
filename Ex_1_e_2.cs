15a6d5415132cbce242d880f07e02e45467c209f
Luis Felipe Roque <161150723+luisfelipeaz@users.noreply.github.com>
2024-10-31 10:13:26 -0300

    Add files via upload

---
+++ Ex_1_e_2.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Biblioteca_Matriz;
+
+class Ex_1_e_2
+{
+    static int maiorValorMatriz(int[,]mat)
+    {
+        int linhas = mat.GetLength(0);//pega linhas
+        int cols = mat.GetLength(1);
+        int maior = mat[0,0];
+        for (int i = 0;i<linhas; i++)
+            for (int j = 0; j < cols; j++)
+                if (mat[i,j]>maior)
+                    maior = mat[i,j];
+
+        //terminou percorrer funcao 
+        return maior;
+    }
+       static int menorValorMatriz(int[,]mat)
+    {
+        int linhas = mat.GetLength(0);//pega linhas
+        int cols = mat.GetLength(1);
+        int menor = mat[0,0];
+        for (int i = 0;i<linhas; i++)
+            for (int j = 0; j < cols; j++)
+                if (mat[i,j]<menor)
+                    menor = mat[i,j];
+
+        //terminou percorrer funcao 
+        return menor;
+    }
+   
+    static void Main()
+    {
+        int cols, linhas;
+        Console.WriteLine("Quantidades de linhas e colunas");
+        linhas = int.Parse(Console.ReadLine());
+        cols = int.Parse(Console.ReadLine());
+        int[,] matrix = new int[linhas, cols];
+        Matriz.gera(matrix);
+        Matriz.mostra(matrix);
+        Console.WriteLine($"O maior numero da matriz:{maiorValorMatriz(matrix)}");
+        Console.WriteLine($"O menor numero da matriz:{menorValorMatriz(matrix)}");
+
+    } 
+}
