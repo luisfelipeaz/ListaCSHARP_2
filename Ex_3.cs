bd876da862f2f8f7e6ef0fb44d7dccc2ad22ed71
Luis Felipe Roque <161150723+luisfelipeaz@users.noreply.github.com>
2024-11-01 09:08:50 -0300

    Add files via upload

---
+++ Ex_3.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Biblioteca_Matriz;
+
+    class Ex_3
+    {
+        static void main()
+        {
+            int cols, linhas;
+            Console.WriteLine("Quantidades de linhas e colunas");
+            linhas = int.Parse(Console.ReadLine());
+            cols = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[linhas, cols];
+            Matriz.gera(matrix);
+            Console.WriteLine("***MATRIZ GERADA***");
+            Matriz.mostra(matrix);
+
+            Console.WriteLine("digite um numero para ver quantas vezes ele aparece");
+            int x = int.Parse(Console.ReadLine());
+
+            int cont = 0;
+
+            for (int i = 0; i < linhas; i++) 
+            { 
+               for(int j = 0; j < cols; j++)
+               {
+                    cont++;
+               }
+            }
+            Console.WriteLine($"O numero {x} aparece {cont} vezes na matriz");
+        }
+    }
+
---
+++ Ex_4_e_5.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Biblioteca_Matriz;
+
+    class Ex_4_e_5
+    {
+        static void MostrarDiagonalPrincipal(int[,] mat)
+        {
+            int linhas = mat.GetLength(0);
+            int cols = mat.GetLength(1);   
+
+            Console.WriteLine("*** ELEMENTOS DA DIAGONAL PRINCIPAL ***");
+            for (int i = 0; i < Math.Min(linhas, cols); i++)
+            {
+                Console.WriteLine($"Elemento na posição [{i}, {i}] = {mat[i, i]}");
+            }
+        }
+        static void MostrarDiagonalSecundaria(int[,] mat)
+        {
+            int linhas = mat.GetLength(0); 
+            int cols = mat.GetLength(1);   
+
+            Console.WriteLine("*** ELEMENTOS DA DIAGONAL SECUNDÁRIA ***");
+            for (int i = 0; i < linhas; i++)
+            {
+                
+                Console.WriteLine($"Elemento na posição [{i}, {cols - 1 - i}] = {mat[i, cols - 1 - i]}");
+            }
+        }
+
+    static void Main() 
+        {
+            int cols, linhas;
+            Console.WriteLine("Quantidades de linhas e colunas");
+            linhas = int.Parse(Console.ReadLine());
+            cols = int.Parse(Console.ReadLine());
+            int[,] matriz = new int[linhas, cols];
+            Matriz.gera(matriz);
+            Matriz.mostra(matriz);
+
+            MostrarDiagonalPrincipal(matriz);
+            MostrarDiagonalSecundaria(matriz);
+        }
+    }
+
+
